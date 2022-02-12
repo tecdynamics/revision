01942031959e99ff74f6510239bb4351eb78ac05
Tec-Dynamics <support@tecdynamics.co.uk>
2022-02-12 22:11:57 +0000

    Add files via upload

---
+++ config/general.php
@@ -0,0 +1,10 @@
+<?php
+
+use Tec\Page\Models\Page;
+
+return [
+    // List supported modules or plugins
+    'supported' => [
+        Page::class,
+    ],
+];
---
+++ src/Providers/RevisionServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tec\Revision\Providers;
+
+use Tec\Base\Traits\LoadAndPublishDataTrait;
+use Illuminate\Support\ServiceProvider;
+
+class RevisionServiceProvider extends ServiceProvider
+{
+    use LoadAndPublishDataTrait;
+
+    public function boot()
+    {
+        $this->setNamespace('packages/revision')
+            ->loadAndPublishViews()
+            ->loadAndPublishConfigurations(['general'])
+            ->loadMigrations()
+            ->publishAssets();
+
+        $this->app->booted(function () {
+            $this->app->register(HookServiceProvider::class);
+        });
+    }
+}
